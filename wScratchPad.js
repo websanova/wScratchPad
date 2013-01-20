05133283e27d22747f153429bfd0cd2d502358bd
websanova <rob@websanova.com>
2013-01-19 19:08:14 -0500

    Updating to version 1.4.2

--- wScratchPad.js
+++ wScratchPad.js
@@ -8,7 +8,7 @@
  * @license         This wScratchPad jQuery plug-in is dual licensed under the MIT and GPL licenses.
  * @link            http://www.websanova.com
  * @github			http://github.com/websanova/wScratchPad
- * @version         Version 1.4.1
+ * @version         Version 1.4.2
  *
  ******************************************/
 (function($)
